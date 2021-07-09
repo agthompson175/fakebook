ba13d67bdc80f9b23d02c91168f61ddf28cbf1b9
agthompson175 <agthompson175@gmail.com>
2021-07-09 11:15:48 -0400

    blog posts

--- app/blueprints/blog/routes.py
+++ app/blueprints/blog/routes.py
@@ -3,14 +3,9 @@ from app.blueprints.blog.models import Post
 from .import bp as app
 
 
-
-
-
-
 @app.route('/post/<int:id>')
 def get_post(id):
     context = {
         'p': Post.query.get(id)
     }
     return render_template('blog-single.html', **context)
-    
